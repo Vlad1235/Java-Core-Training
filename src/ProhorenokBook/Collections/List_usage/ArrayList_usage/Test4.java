6ae46479f50ce5955625e4e4bb3b82c8aee30b0e
Vlad1235 <fn1235@bk.ru>
2019-10-04 03:48:12 +0900

    03.10.19

--- src/ProhorenokBook/Collections/List_usage/ArrayList_usage/Test4.java
+++ src/ProhorenokBook/Collections/List_usage/ArrayList_usage/Test4.java
@@ -1,5 +1,6 @@
 package ProhorenokBook.Collections.List_usage.ArrayList_usage;
 
+import javax.sound.midi.Soundbank;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -29,5 +30,27 @@ public class Test4 {
         Collections.addAll(arr2, 1,4,5,6,72,67,24);
         System.out.println(arr2.remove(0)); // 1
         System.out.println(arr2.toString()); // [4, 5, 6, 72, 67, 24]
+
+
+        /*
+        Удаление из списка все элементы, которые соответсвтуют значениям другой коллекции
+         */
+        List<Integer> arr3 = new ArrayList<Integer>();
+        Collections.addAll(arr3,1,2);
+        List<Integer> arr4 = new ArrayList<Integer>();
+        Collections.addAll(arr4, 1,2,3,2);
+        System.out.println(arr4.toString());//[1, 2, 3, 2]
+        System.out.println(arr4.removeAll(arr3)); // true - удаляем из arr4 все значения arr3
+        System.out.println(arr4.toString());//[3]
+
+
+        /*
+        Удаление определенных значений из списка
+         */
+        List<Integer> arr5 = new ArrayList<Integer>();
+        Collections.addAll(arr5,1,2,3,4,5,6,7,8,9);
+        System.out.println(arr5.toString());// [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        System.out.println(arr5.removeIf(elem ->elem<5)); // true - удалим все элементы значения которых меньше 5
+        System.out.println(arr5.toString()); //[5, 6, 7, 8, 9]
     }
 }
